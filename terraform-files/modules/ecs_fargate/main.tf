66eb2223bfc7fe5013a2c268fe8b667d4f11c34f
boopathy-turtil <boopathy.s@turtil.co>
2025-06-21 12:32:37 +0530

    test

--- terraform-files/modules/ecs_fargate/main.tf
+++ terraform-files/modules/ecs_fargate/main.tf
@@ -58,8 +58,8 @@ resource "aws_lb_target_group" "this" {
   health_check {
     path                = "/"
     interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
+    timeout             = 15
+    healthy_threshold   = 5
     unhealthy_threshold = 2
     matcher             = "200"
   }
